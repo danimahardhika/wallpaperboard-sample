80f94930308fd82af9e06771ac62b82f0ba184fe
Dani Mahardhika <danimahardhika112@gmail.com>
2017-10-30 18:12:01 +0700

    Update MainActivity.java

--- app/src/main/java/com/dm/wallpaper/board/sample/activities/MainActivity.java
+++ app/src/main/java/com/dm/wallpaper/board/sample/activities/MainActivity.java
@@ -1,18 +1,20 @@
 package com.dm.wallpaper.board.sample.activities;
 
-import android.os.Bundle;
+import android.support.annotation.NonNull;
 
 import com.dm.wallpaper.board.activities.WallpaperBoardActivity;
-import com.dm.wallpaper.board.sample.licenses.License;
+import com.dm.wallpaper.board.activities.configurations.ActivityConfiguration;
+import com.dm.wallpaper.board.demo.licenses.License;
 
 public class MainActivity extends WallpaperBoardActivity {
 
+    @NonNull
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        initMainActivity(savedInstanceState,
-                License.isLicenseCheckerEnabled(),
-                License.getRandomString(),
-                License.getLicenseKey(),
-                License.getDonationProductsId());
+    public ActivityConfiguration onInit() {
+        return new ActivityConfiguration()
+                .setLicenseCheckerEnabled(License.isLicenseCheckerEnabled())
+                .setLicenseKey(License.getLicenseKey())
+                .setRandomString(License.getRandomString())
+                .setDonationProductsId(License.getDonationProductsId());
     }
 }
